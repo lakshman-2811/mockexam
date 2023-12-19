817aa318b4228b20bf18d45740009976ed893d09
lakshman-2811 <dontaganilakshman@gmail.com>
2023-12-19 16:48:33 +0530

    Initial Commit

---
+++ PractiseExam/src/main/java/com/acc/lkm/PractisExam/App.java
@@ -0,0 +1,15 @@
+package com.acc.lkm.PractisExam;
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	TreeMap<String,String> tm = new TreeMap<String,String>();
+    	tm.put("Java", "proj1");
+    	tm.put("Python", "proj2");
+    	tm.put("SAP", "proj3");
+    	tm.put("SFDC", "proj4");
+    	tm.put("SQL", "proj5");
+    	System.out.println(tm);
+    }
+}
---
+++ PractiseExam/src/main/java/com/acc/lkm/PractisExam/TreemapDemo.java
@@ -0,0 +1,10 @@
+package com.acc.lkm.PractisExam;
+
+public class TreemapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
